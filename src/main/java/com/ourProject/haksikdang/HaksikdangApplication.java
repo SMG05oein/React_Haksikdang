5f92c2a6708ff5a4355f38a3f49525ab04645498
SMG <SMG05eoin@gmail.com>
2025-05-29 00:16:06 +0900

    first commit

---
+++ src/main/java/com/ourProject/haksikdang/HaksikdangApplication.java
@@ -0,0 +1,13 @@
+package com.ourProject.haksikdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaksikdangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaksikdangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ourProject/haksikdang/HaksikdangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourProject.haksikdang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaksikdangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
