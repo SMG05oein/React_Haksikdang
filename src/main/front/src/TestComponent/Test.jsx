b1c28040755a14d4a31c44acb9ed3a08d73fb994
SMG <SMG05eoin@gmail.com>
2025-05-29 15:19:12 +0900

    백엔드 - 탑, 홈페이지 틀 완성(탑메뉴에 드랍다운 메뉴 좀 이상하게 보임)

--- src/main/front/src/TestComponent/Test.jsx
+++ src/main/front/src/TestComponent/Test.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const Test = () => {
     return (
         <div>
-            벡엔드 작업 확인을 위한 test공간
-            근데 이친구는 스프링이 빌드되어야 원하는 결과 도출
-            <button type={"button"} onClick={()=> {window.location.href = "/"}}>어디로든 문</button>
+            벡엔드 작업 확인을 위한 test공간 <br/>
+            근데 이친구는 스프링이 빌드되어야 원하는 결과 도출 <br/>
+            <button type={"button"} onClick={() => {
+                window.location.href = "/"
+            }}>어디로든 문
+            </button>
         </div>
     );
 };
--- src/main/front/src/Topmenu/TopMenu.jsx
+++ src/main/front/src/Topmenu/TopMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./TopMenu.style.css"
 import {Button, Container, Form, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {Outlet} from "react-router-dom";
@@ -6,17 +6,19 @@ import BuLogo from "../BuLogo.png"
 import { IoPersonCircleOutline } from "react-icons/io5";
 
 const TopMenu = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+
     return (
         <div className="top-menu">
             <Navbar expand="lg" className="bg-primary bg-gradient">
                 <Container fluid>
-                    <Navbar.Brand href="/"><img style={{width:'44px'}} src={BuLogo}/> 백석대 통합 학식당 예매 시스템</Navbar.Brand>
+                    <Navbar.Brand href="/"><img style={{width:'44px'}} src={BuLogo}/> 백석대 학식당 통합 예매 시스템</Navbar.Brand>
                     {/*<IoPersonCircleOutline style={{ width: "44", height: "44", marginLeft: "auto" }} />*/}
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
                             className="me-auto my-2 my-lg-0"
-                            style={{ maxHeight: '1100px' }}
+                            style={{ maxHeight: '1100px', width: '100%' }}
                             navbarScroll
                         >
                             <Nav.Link href="/">홈으로</Nav.Link>
@@ -24,7 +26,18 @@ const TopMenu = () => {
                             <Nav.Link href="/">밀겨울</Nav.Link>
                             <Nav.Link href="/">안골</Nav.Link>
                             <Nav.Link href="/devtest">DevTest</Nav.Link>
-                            <IoPersonCircleOutline onClick={()=>{window.location.href = '/myPage'}} className={'person-icon'}  />
+
+                            <NavDropdown
+                                title={<IoPersonCircleOutline className="person-icon"/>}
+                                // id="person-dropdown"
+                                className="dropdown-person-icon"
+                                show={showDropdown}
+                                onMouseEnter={() => setShowDropdown(true)}
+                                onMouseLeave={() => setShowDropdown(false)}
+                            >
+                                <NavDropdown.Item href="/">로그인</NavDropdown.Item>
+                                <NavDropdown.Item href="/">회원가입</NavDropdown.Item>
+                            </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
